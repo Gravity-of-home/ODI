143247198e16eca9aa3dc05072c7f91c3eb26aa0
Soomin Kim <min120926@gmail.com>
2024-04-18 17:28:47 +0900

    🐛 Fix: [BE] fix: 불필요한 어노테이션 제거

--- Backend/src/main/java/com/homegravity/Odi/global/config/QueryDSLConfig.java
+++ Backend/src/main/java/com/homegravity/Odi/global/config/QueryDSLConfig.java
@@ -8,11 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class QueryDSLConfig {
 
     @PersistenceContext
-    private final EntityManager entityManager;
+    private EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
