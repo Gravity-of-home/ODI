9243c0accb933ff6263ad17ea5626fa32d6dd3d7
Soomin Kim <min120926@gmail.com>
2024-04-18 17:26:28 +0900

    ✏️ Chore: [BE] feat: 동시성 문제 방지

--- Backend/src/main/java/com/homegravity/Odi/global/config/QueryDSLConfig.java
+++ Backend/src/main/java/com/homegravity/Odi/global/config/QueryDSLConfig.java
@@ -2,6 +2,7 @@ package com.homegravity.Odi.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class QueryDSLConfig {
+
+    @PersistenceContext
     private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
     }
-
 }
