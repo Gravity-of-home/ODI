21ae760da4e03d9041c7fe1b2006144a3b842b65
Soomin Kim <min120926@gmail.com>
2024-04-23 17:12:49 +0900

    ✨ Feat: [BE] Party, PartyBoardStats Entity 추가
    - 파티 구인 글 CRUD를 위한 Entity 구현

---
+++ Backend/src/main/java/com/homegravity/Odi/domain/party/entity/Party.java
@@ -0,0 +1,118 @@
+package com.homegravity.Odi.domain.party.entity;
+
+import com.homegravity.Odi.global.entity.BaseBy;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE party SET deleted_at = NOW() where party_id = ?")
+public class Party extends BaseBy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "party_id")
+    private Long id;
+
+    @Column(name = "title", length = 50)
+    private String title;
+
+    @Column(name = "departures_name", length = 50)
+    private String departuresName;
+
+    @Column(name = "departures_latitude")
+    private String departuresLatitude;
+
+    @Column(name = "departures_longitude")
+    private String departuresLongitude;
+
+    @Column(name = "arrivals_name", length = 50)
+    private String arrivalsName;
+
+    @Column(name = "arrivals_latitude")
+    private String arrivalsLatitude;
+
+    @Column(name = "arrivals_longitude")
+    private String arrivalsLongitude;
+
+    @Column(name = "expected_cost")
+    private int expectedCost;
+
+    @Column(name = "departures_date")
+    private LocalDateTime departuresDate;
+
+    @Column(name = "max_participants")
+    private int maxParticipants;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "gender")
+    private boolean gender;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @OneToOne(mappedBy = "party", cascade = CascadeType.REMOVE)
+    private PartyBoardStats partyBoardStats;
+
+    @Builder
+    private Party(String title, String departuresName, String departuresLatitude, String departuresLongitude,
+                  String arrivalsName, String arrivalsLatitude, String arrivalsLongitude,
+                  int expectedCost, LocalDateTime departuresDate, int maxParticipants,
+                  String category, boolean gender, String state, String content) {
+        this.title = title;
+        this.departuresName = departuresName;
+        this.departuresLatitude = departuresLatitude;
+        this.departuresLongitude = departuresLongitude;
+        this.arrivalsName = arrivalsName;
+        this.arrivalsLatitude = arrivalsLatitude;
+        this.arrivalsLongitude = arrivalsLongitude;
+        this.expectedCost = expectedCost;
+        this.departuresDate = departuresDate;
+        this.maxParticipants = maxParticipants;
+        this.category = category;
+        this.gender = gender;
+        this.state = state;
+        this.content = content;
+    }
+
+    public static Party of(String title, String departuresName, String departuresLatitude, String departuresLongitude,
+                           String arrivalsName, String arrivalsLatitude, String arrivalsLongitude,
+                           int expectedCost, LocalDateTime departuresDate, int maxParticipants,
+                           String category, boolean gender, String state, String content) {
+
+        return Party.builder()
+                .title(title)
+                .departuresName(departuresName)
+                .departuresLatitude(departuresLatitude)
+                .departuresLongitude(departuresLongitude)
+                .arrivalsName(arrivalsName)
+                .arrivalsLatitude(arrivalsLatitude)
+                .arrivalsLongitude(arrivalsLongitude)
+                .expectedCost(expectedCost)
+                .departuresDate(departuresDate)
+                .maxParticipants(maxParticipants)
+                .category(category)
+                .gender(gender)
+                .state(state)
+                .content(content)
+                .build();
+
+    }
+
+    public void updatePartyBoardStats(PartyBoardStats partyBoardStats) {
+        this.partyBoardStats = partyBoardStats;
+    }
+
+}
