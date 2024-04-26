ac5d5aaa35579e2e881f0ddb220a8b82159ed3be
oistmil <oistmil@gmail.com>
2024-04-26 14:52:19 +0900

    ✨ Feat: [BE] Swagger 로그인 추가

--- Backend/src/main/java/com/homegravity/Odi/global/config/SwaggerConfig.java
+++ Backend/src/main/java/com/homegravity/Odi/global/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.homegravity.Odi.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -19,30 +23,30 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
 
-//        SecurityRequirement addSecurityItem = new SecurityRequirement();
-//        addSecurityItem.addList("Authorization");
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Authorization");
 
         return new OpenAPI()
-//                .components(components())
-//                .addSecurityItem(addSecurityItem)
+                .components(components())
+                .addSecurityItem(addSecurityItem)
                 .info(apiInfo());
     }
-//
-//    private Components components() {
-//
-//        return new Components()
-//                .addSecuritySchemes(HttpHeaders.AUTHORIZATION, new SecurityScheme()
-//                        .type(SecurityScheme.Type.HTTP)
-//                        .scheme("bearer")
-//                        .bearerFormat("JWT")
-//                        .name(HttpHeaders.AUTHORIZATION)
-//                        .in(SecurityScheme.In.HEADER))
-//                .addSecuritySchemes("RefreshToken", new SecurityScheme()
-//                        .type(SecurityScheme.Type.HTTP)
-//                        .scheme("cookie")
-//                        .name("RefreshToken")
-//                        .in(SecurityScheme.In.COOKIE));
-//    }
+
+    private Components components() {
+
+        return new Components()
+                .addSecuritySchemes(HttpHeaders.AUTHORIZATION, new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .name(HttpHeaders.AUTHORIZATION)
+                        .in(SecurityScheme.In.HEADER))
+                .addSecuritySchemes("RefreshToken", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("cookie")
+                        .name("RefreshToken")
+                        .in(SecurityScheme.In.COOKIE));
+    }
 
     private Info apiInfo() {
 
