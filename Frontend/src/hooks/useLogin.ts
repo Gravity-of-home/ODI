f1d9a5977bd3178f7b1c064a3aba3dfb55c88046
김태용 <94teo@naver.com>
2024-05-03 11:05:42 +0900

    ✨ Feat: Add constants and hooks for bottom sheet functionality

--- Frontend/src/hooks/useLogin.ts
+++ Frontend/src/hooks/useLogin.ts
@@ -1,12 +1,11 @@
 import { getUserInfo } from '@/apis/User';
 import { useQuery } from '@tanstack/react-query';
-import useUserStore from '@/stores/useUserStore';
 
 export const useLogin = () => {
   const useGetUserInfo = (props: { userId: string }) => {
     return useQuery({
       queryKey: ['user'],
-      queryFn: () => getUserInfo(props.userId),
+      queryFn: () => getUserInfo(),
     });
   };
 
