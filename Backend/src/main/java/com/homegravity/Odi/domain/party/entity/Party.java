27e0ad71d8de57e7140de437cb26ab68956a4e7b
oistmil <oistmil@gmail.com>
2024-05-03 17:26:20 +0900

    ✨ Feat: 정산 및 포인트 이용 관련 Entity 추가

--- Backend/src/main/java/com/homegravity/Odi/domain/party/entity/Party.java
+++ Backend/src/main/java/com/homegravity/Odi/domain/party/entity/Party.java
@@ -65,6 +65,9 @@ public class Party extends BaseBy {
     @OneToOne(mappedBy = "party", cascade = CascadeType.ALL)
     private PartyBoardStats partyBoardStats;
 
+    @OneToOne(mappedBy = "party", cascade = CascadeType.ALL)
+    private PartySettlement partySettlement;
+
     @Builder
     private Party(String title, String departuresName, Point departuresLocation,
                   String arrivalsName, Point arrivalsLocation,
@@ -117,6 +120,10 @@ public class Party extends BaseBy {
 
     public void updatePartyBoardStats(PartyBoardStats partyBoardStats) {
         this.partyBoardStats = partyBoardStats;
+    }
+
+    public void updatePartySettlement(PartySettlement partySettlement) {
+        this.partySettlement = partySettlement;
     }
 
     public void updateDeparturesName(String departuresName) {
