20b7f376121d59b2caa66785d136539648f27441
Soomin Kim <min120926@gmail.com>
2024-05-07 00:24:32 +0900

    🐛 Fix: 웹 소켓 연결 오류 수정

--- Backend/src/main/java/com/homegravity/Odi/global/jwt/filter/JWTFilter.java
+++ Backend/src/main/java/com/homegravity/Odi/global/jwt/filter/JWTFilter.java
@@ -9,19 +9,22 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.util.AntPathMatcher;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Optional;
 
+@Slf4j
+@Component
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
