81cbc4fa7d89a6f376073670c6a6392b9069b32c
Soomin Kim <min120926@gmail.com>
2024-05-07 16:01:52 +0900

    ✏️ Chore: http method 수정

--- Backend/src/main/java/com/homegravity/Odi/domain/party/controller/PartyController.java
+++ Backend/src/main/java/com/homegravity/Odi/domain/party/controller/PartyController.java
@@ -63,7 +63,7 @@ public class PartyController {
     }
 
     @Operation(summary = "파티 채팅 정보 조회", description = "파티 정보를 채팅방에 제공합니다.")
-    @PostMapping("/{party-id}/chat-info")
+    @GetMapping("/{party-id}/chat-info")
     public ApiResponse<PartyChatInfoResponseDTO> getPartyChatInfo(@PathVariable(value = "party-id") Long partyId, @AuthenticationPrincipal Member member) {
         return ApiResponse.of(SuccessCode.PARTY_GET_SUCCESS, partyService.getPartyChatInfo(partyId, member));
     }
