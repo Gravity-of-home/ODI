89e3fc6fca48c738bcf322581169dc753f3f4cca
suyeon-hyeon <shonee99@naver.com>
2024-05-07 14:50:32 +0900

    ✨ Feat: S3 설정 추가 - build.gradle 추가 - yml 추가 - S3Config 추가 - S3Service 추가

---
+++ Backend/src/main/java/com/homegravity/Odi/global/service/S3Service.java
@@ -0,0 +1,41 @@
+package com.homegravity.Odi.global.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    /*
+    S3 파일 업로드
+     */
+    public String saveFile(MultipartFile multipartFile) throws IOException {
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket, originalFilename).toString();
+    }
+
+    /*
+    S3 버킷 파일 삭제
+     */
+    public void deleteFile(String originalFilename)  {
+        amazonS3.deleteObject(bucket, originalFilename);
+    }
+}
