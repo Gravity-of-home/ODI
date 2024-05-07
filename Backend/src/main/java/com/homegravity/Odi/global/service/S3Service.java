92dea3304ebfeeafc20a88f9ceb9df7d51a5b343
suyeon-hyeon <shonee99@naver.com>
2024-05-07 15:13:44 +0900

    🐛 Fix: S3Service 예외처리 추가

--- Backend/src/main/java/com/homegravity/Odi/global/service/S3Service.java
+++ Backend/src/main/java/com/homegravity/Odi/global/service/S3Service.java
@@ -2,13 +2,18 @@ package com.homegravity.Odi.global.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.homegravity.Odi.global.response.error.ErrorCode;
+import com.homegravity.Odi.global.response.error.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class S3Service {
@@ -21,15 +26,25 @@ public class S3Service {
     /*
     S3 파일 업로드
      */
-    public String saveFile(MultipartFile multipartFile) throws IOException {
+    public String saveFile(MultipartFile multipartFile) {
+        // 파일 원본 이름
         String originalFilename = multipartFile.getOriginalFilename();
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(multipartFile.getSize());
-        metadata.setContentType(multipartFile.getContentType());
+        try {
+            // S3 메타데이터 설정
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(multipartFile.getSize());
+            metadata.setContentType(multipartFile.getContentType());
 
-        amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
-        return amazonS3.getUrl(bucket, originalFilename).toString();
+            // 버킷에 파일 저장
+            amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+            // 파일 주소 반환
+            return amazonS3.getUrl(bucket, originalFilename).toString();
+        } catch (IOException e) {
+            log.info("S3 파일 저장 실퍠");
+            throw new BusinessException(ErrorCode.S3_PROCESS_ERROR, ErrorCode.S3_PROCESS_ERROR.getMessage());
+        }
+
     }
 
     /*
