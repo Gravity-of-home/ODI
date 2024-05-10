f29843d6567b3d468d6c8fb8939a97375e0d2423
suyeon-hyeon <shonee99@naver.com>
2024-05-10 13:40:07 +0900

    🐛 Fix: Subscribe 시 입장문구 pub 메세지 삭제

--- Backend/src/main/java/com/homegravity/Odi/global/handler/StompHandler.java
+++ Backend/src/main/java/com/homegravity/Odi/global/handler/StompHandler.java
@@ -39,7 +39,6 @@ public class StompHandler implements ChannelInterceptor {
         Member sender = memberRepository.findById(Long.valueOf(jwtUtil.getId(token)))
                 .orElseThrow(()-> new BusinessException(ErrorCode.MEMBER_ID_NOT_EXIST,ErrorCode.MEMBER_ID_NOT_EXIST.getMessage()));
         String nickname = sender.getNickname();
-        String image = sender.getImage();
         if (StompCommand.CONNECT == accessor.getCommand()) { // websocket 연결요청
             log.info("!!!CONNECT!!! {}", sender.getNickname());
             // Header의 jwt token 검증
@@ -47,22 +46,22 @@ public class StompHandler implements ChannelInterceptor {
             // header정보에서 구독 destination정보를 얻고, roomId를 추출한다.
             String roomId = chatService.getRoomId(Optional.ofNullable((String) message.getHeaders().get("simpDestination")).orElse("InvalidRoomId"));
             // 채팅방에 들어온 클라이언트 sessionId를 roomId와 맵핑해 놓는다.(나중에 특정 세션이 어떤 채팅방에 들어가 있는지 알기 위함)
-            String sessionId = (String) message.getHeaders().get("simpSessionId");
-            log.info("매핑이 되니????? {}",sessionId);
-            chatRoomRepository.setUserEnterInfo(sessionId, roomId);
+//            String sessionId = (String) message.getHeaders().get("simpSessionId");
+//            log.info("매핑이 되니????? {}",sessionId);
+//            chatRoomRepository.setUserEnterInfo(sessionId, roomId);
             // 클라이언트 입장 메시지를 채팅방에 발송한다.(redis publish)
-            log.info("??!?!?!?!?!?!?!? {}", nickname);
-            chatService.sendChatMessage(ChatMessageDTO.builder().roomId(roomId).senderNickname(nickname).senderImage(image).type(MessageType.ENTER).build());
+//            chatService.sendChatMessage(ChatMessageDTO.builder().roomId(roomId).senderNickname(nickname).senderImage(image).type(MessageType.ENTER).build());
             log.info("SUBSCRIBED {}, {}", nickname, roomId);
         } else if (StompCommand.DISCONNECT == accessor.getCommand()) { // Websocket 연결 종료
             // 연결이 종료된 클라이언트 sesssionId로 채팅방 id를 얻는다.
-            String sessionId = (String) message.getHeaders().get("simpSessionId");
-            String roomId = chatRoomRepository.getUserEnterRoomId(sessionId);
+//            String sessionId = (String) message.getHeaders().get("simpSessionId");
+//            String roomId = chatRoomRepository.getUserEnterRoomId(sessionId);
             // 클라이언트 퇴장 메시지를 채팅방에 발송한다.(redis publish)
-            chatService.sendChatMessage(ChatMessageDTO.builder().roomId(roomId).senderNickname(nickname).senderImage(image).type(MessageType.QUIT).build());
+//            chatService.sendChatMessage(ChatMessageDTO.builder().roomId(roomId).senderNickname(nickname).senderImage(image).type(MessageType.QUIT).build());
             // 퇴장한 클라이언트의 roomId 맵핑 정보를 삭제한다.
-            chatRoomRepository.removeUserEnterInfo(sessionId);
-            log.info("DISCONNECTED {}, {}", sessionId, roomId);
+//            chatRoomRepository.removeUserEnterInfo(sessionId);
+//            log.info("DISCONNECTED {}, {}", sessionId, roomId);
+            log.info("DISCONNECTED~~~~");
         } else if (StompCommand.UNSUBSCRIBE == accessor.getCommand()) { // 채팅룸 구독취소
             log.info("!!!UNSUBSCRIBED!!!");
         }
