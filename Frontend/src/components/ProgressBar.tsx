4fda6df13e79ca4f6b8b5383371cea512da25779
김태용 <94teo@naver.com>
2024-05-16 21:40:27 +0900

    ✨ Feat: 이용내역 추가

--- Frontend/src/components/ProgressBar.tsx
+++ Frontend/src/components/ProgressBar.tsx
@@ -26,7 +26,7 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ targetValue }) => {
   return (
     <div className='w-[100%] bg-slate-700 h-4 rounded-full'>
       <div
-        className='bg-OD_PINK h-4 rounded-full transition-width duration-500 flex justify-center items-center text-white font-bold text-sm'
+        className='bg-OD_PURPLE h-4 rounded-full transition-width duration-500 flex justify-center items-center text-white font-bold text-sm'
         style={{ width: `${progress}%` }}>
         {progress}
       </div>
